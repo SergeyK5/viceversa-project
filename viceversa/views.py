6a95b56252e6b433bfc3038d2f8140e4c72d2321
Sergey <sergeykraev455@mail.ru>
2023-10-22 22:08:02 +0300

    CountWords

--- viceversa/views.py
+++ viceversa/views.py
@@ -6,5 +6,6 @@ def home(request):
 def reverse(request):
 	user_text = request.GET['usertext']
 	reverse_text = user_text[::-1]
-	return render(request, 'reverse.html', {'usertext':user_text,'reversetext':reverse_text})
+	len_w = len(user_text.split())
+	return render(request, 'reverse.html', {'usertext':user_text,'reversetext':reverse_text, 'lenW': len_w})
 
